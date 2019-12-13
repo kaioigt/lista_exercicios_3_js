c09fd2341f08f715ac799e9145848705677c2c2f
kaioigt <34192886+kaioigt@users.noreply.github.com>
2019-12-13 11:16:46 -0300

    Add files via upload

---
+++ Lista exercicio/x2/js/maior.js
@@ -0,0 +1,24 @@
+var numero = [''];
+
+let maior = 0;
+
+
+for (var i = 0; i < 5 ; i++) {
+	numero[i] = prompt('Informe o numero ');
+
+	if (numero[i]>maior) {
+		maior=numero[i];
+	}
+
+}
+
+alert(maior);
+
+
+
+
+
+	
+
+
+
